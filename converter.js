83ffe0cdea39fed1a01c3ee1b0fbc6fbd718b59a
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-03-12 15:58:55 +0100

    Add comments

--- converter.js
+++ converter.js
@@ -4,19 +4,28 @@ const sourcePath = './csv-file'
 const resultPath = './resulting-md-files'
 const files = fs.readdirSync(sourcePath)
 
-function createMd(data){
-    data.tags = data.tags.match(/\b(\w+)\b/g)
+// Create the resultPath in case it doesn't exist
+if (!fs.existsSync(resultPath)){
+    fs.mkdirSync(resultPath);
+}
+
+// Use the object from csv-parser to create a hugo readable md
+function createMd(data) {
+    // Parsing tags since they usually come in this format: "tag1, tag2, tag3"
+    if(data.tags)
+        data.tags = data.tags.match(/\b(\w+)\b/g)
     fs.writeFileSync(`${resultPath}/${data.title}.md`, JSON.stringify(data, null, 2))
 }
 
-if( !files[0] || files[0].indexOf('.csv') === -1){
-    console.warn('No .csv file detected')
-}
-else{
-    fs.createReadStream(sourcePath + '/' + files[0])
-    .pipe(csv())
-    .on('data', data => createMd(data))
-    .on('end', () => { console.log('DONE') })
-}
-    
+// This is to ignore the .gitkeep and allow multiple .csv
+files.forEach(file => {
+    if (file.indexOf('.csv') !== -1) {
+        fs.createReadStream(sourcePath + '/' + file)
+            .pipe(csv())
+            .on('data', data => createMd(data))
+            .on('end', () => { console.log('DONE') })
+    }
+})
+
+
 
